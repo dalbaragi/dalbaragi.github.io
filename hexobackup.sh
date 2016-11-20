d3d7991c3d30d428ba6c72b56106765e3621973f
dalbaragi <lrh1992@yeah.net>
2016-11-20 13:58:50 +0800

    201611201358

--- hexobackup.sh
+++ hexobackup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #将Hexo文件夹下的内容备份并部署到GitHub
+echo"Hexo backup............"
 git add .
 git commit -m "$1"
 git push origin hexo
+echo"-------------------------------------------------------------"
+echo"Hexo deploy............"
 hexo g -d
+echo"-------------------------------------------------------------"
